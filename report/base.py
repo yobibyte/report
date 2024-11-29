1ed97e51bbcf9ae64c03a9768cc754ba1650809d
yobibyte <y0b1byte@proton.me>
2024-11-29 21:39:42 +0000

    Add templating.

--- report/base.py
+++ report/base.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
+from jinja2 import Template
+import argparse
+from datetime import datetime
+import re
 
 
-class Report(ABC):
+class AbstractReport(ABC):
     def __init__(self, title):
         self._title = title
 
@@ -21,3 +25,40 @@ class Report(ABC):
         self.load_data()
         self.prepare()
         self.save()
+
+
+REPORT_TEMPLATE = """from report.base import AbstractReport
+
+
+class Report(AbstractReport):
+    
+    def load_data(self):
+        raise NotImplementedError
+
+    def prepare(self):
+        raise NotImplementedError
+
+if __name__ == "__main__":
+    report = Report(title="{{title}}")
+    report.generate()
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate a report template.")
+    parser.add_argument(
+        "title",
+        type=str,
+        help="Report title",
+    )
+    args = parser.parse_args()
+    title = re.sub(r"[./ ]", "_", args.title)
+    template = Template(REPORT_TEMPLATE)
+    report_code = template.render(title=args.title)
+    date = datetime.now().strftime("%Y_%m_%d")
+    with open(f"{date}_{title}.py", "w") as f:
+        f.write(report_code)
+
+
+if __name__ == "__main__":
+    main()
